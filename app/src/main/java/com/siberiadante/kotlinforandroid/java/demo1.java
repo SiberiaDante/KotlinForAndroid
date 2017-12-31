4995c769da848918ff04e236071abd0d5e6cb0e1
SiberiaDante <994537867@qq.com>
2017-12-31 17:44:41 +0800

    Create Project

---
+++ app/src/main/java/com/siberiadante/kotlinforandroid/java/demo1.java
@@ -0,0 +1,14 @@
+package com.siberiadante.kotlinforandroid.java;
+
+/**
+ * @Created： SiberiaDante
+ * @Date： 2017/12/31
+ * @Describe：
+ * @Github： https://github.com/SiberiaDante
+ * @Blog： http://www.cnblogs.com/shen-hua/
+ */
+
+public class demo1 {
+    String name;
+    String name1="SiberiaDante";
+}
---
+++ app/src/main/java/com/siberiadante/kotlinforandroid/kotlin/demo1.kt
@@ -0,0 +1,12 @@
+package com.siberiadante.kotlinforandroid.kotlin
+
+/**
+ * @Created： SiberiaDante
+@Date： 2017/12/31
+@Describe：
+@Github： https://github.com/SiberiaDante
+@Blog： http://www.cnblogs.com/shen-hua/
+ */
+class demo1{
+    var namne = null;
+}
