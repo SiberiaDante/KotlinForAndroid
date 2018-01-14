4ecd9bafb91d27667a8bfa6cf3fbc6ef6b5396c8
SiberiaDante <994537867@qq.com>
2018-01-14 22:54:04 +0800

    从零开始学Kotlin-控制语句（4）

--- app/src/main/java/com/siberiadante/kotlinforandroid/kotlin/demo4.kt
+++ app/src/main/java/com/siberiadante/kotlinforandroid/kotlin/demo4.kt
@@ -1,26 +1,18 @@
-从零开始学Kotlin-控制语句（4）
-## 条件控制-if
-```
-        var a=10
-        var b=20
-        if(a>b) print(a)
-        if(a>b){
-            print(a)
-        }else{
-            print(b)
-        }
-        val max = if( a > b) a else b
-        if ( a in 1..10){
-            print("a在1到10区间内")
-        }
-```
+package com.siberiadante.kotlinforandroid.kotlin
 
-## 条件控制-when
-```
+/**
+ * @Created： SiberiaDante
+@Date： 2018/1/14
+@Describe：
+@Github： https://github.com/SiberiaDante
+@Blog： http://www.cnblogs.com/shen-hua/
+ */
+class demo4 {
+    fun test() {
         val x = 3
         when (x) {
-            1 -> print("x 是1")
-            2 -> print("x 是2")
+            1 -> print(1)
+            2 -> print(2)
             else -> print("x 不是1也不是2")
         }
         when (x) {
@@ -32,9 +24,9 @@
             !in 5..10 -> print("x不在5-10中")
             else -> print("x 不是1也不是2")
         }
-```
-## 循环控制-for
-```
+    }
+
+    fun test1() {
         val ints = arrayOf(1, 2, 3, 4, 5)
         for (num: Int in ints) {//循环遍历数值
             print((num))
@@ -42,10 +34,9 @@
         for (index in ints.indices) {//循环遍历索引和值
             print("$index 位置的数是 ${ints[index]}")
         }
-```
+    }
 
-## 循环控制-while/do...while
-```
+    fun test2() {
         var a = 10
         while (a < 10) {
             print("a < 10")
@@ -55,5 +46,14 @@
             print("先输出一下再去判断")
             a++
         } while (a < 10)
-```
-## 跳转表达式 return、break、continue
+    }
+
+    fun test3() {
+        val a = 5
+        for (i in 1..10) {
+            if (i == 3) continue//i==3时跳出该循环执行下一轮循环
+            print(i)
+            if (i > 5) break//i>5时跳出循环或者说终止循环
+        }
+    }
+}
